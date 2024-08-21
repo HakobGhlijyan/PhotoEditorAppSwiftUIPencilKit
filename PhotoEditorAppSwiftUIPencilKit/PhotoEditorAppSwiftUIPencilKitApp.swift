7255c509854378c6a890475aff9bbce615a8b39a
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-08-21 17:41:23 +0400

    Initial Commit

---
+++ PhotoEditorAppSwiftUIPencilKit/PhotoEditorAppSwiftUIPencilKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppSwiftUIPencilKitApp.swift
+//  PhotoEditorAppSwiftUIPencilKit
+//
+//  Created by Hakob Ghlijyan on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppSwiftUIPencilKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
